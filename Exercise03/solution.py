632ac62510a3a2a32651ac0c28a67b489a25702f
kourosh.naderi@aalto.fi <kourosh.naderi@aalto.fi>
2019-11-25 23:03:12 +0200

    uses classes

--- Exercise03/solution.py
+++ Exercise03/solution.py
@@ -13,25 +13,51 @@ Options:
 from docopt import docopt
 import pandas as pd
 
-def FetchLines(filename):
-    with open(filename) as f:
-        out = f.read().splitlines()
-    return out
+
+class UrlHandler:
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.url_df_handle = pd.read_csv(url_file, header=None, encoding='utf-8', delimiter='\n')
+        self.index_on_df = 0
+
+    def fetch_first(self):
+        out = ""
+        if self.url_df_handle.count()[0] > self.index_on_df:
+            out = self.url_df_handle.iloc[self.index_on_df][0]
+            self.index_on_df += 1
+        return out
+
+    def has_data(self):
+        return self.url_df_handle.count()[0] > self.index_on_df
+
+
+class FileHandler:
+    @staticmethod
+    def fetch_lines(filename):
+        with open(filename) as f:
+            out = f.read().splitlines()
+        return out
+
 
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='DEMO 1.0')
     # I assume the arguments are the files containing urls and regex
     regex_list = []
-    url_list = []
+    url_handler = None
     flag_main_loop = False
     if arguments['--url'] and arguments['--regex']:
         url_file = arguments['--url']
         regex_file = arguments['--regex']
-        # I am reading the files and keeping a list of [urls,regex] to process, however if url_file is a big data
-        # using pandas is a better solution, not fetching all the info into memory
-        url_list = df = pd.read_csv(url_file, header=None, encoding='utf-8', delimiter='\n')
-        regex_list = FetchLines(regex_file)
+
+        # I use pandas for reading urls to handle huge amount of urls in the big data file
+        url_handler = UrlHandler(url_file)
+        # I read the whole regex file as it is not expected to have huge amount of data entries
+        regex_list = FileHandler.fetch_lines(regex_file)
         flag_main_loop = True
 
     while flag_main_loop:
-        t = 0
+        v = url_handler.fetch_first()
+        print(v)
+
+        if not url_handler.has_data():
+            flag_main_loop = False
